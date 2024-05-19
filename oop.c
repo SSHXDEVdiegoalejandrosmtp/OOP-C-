b7e10e266c6ffda81d2348b769e855d1963c5156
xzyAlejandroSMTP <sshxdevdiegoalejandrogo@gmail.com>
2024-05-19 19:33:27 +0100

    happy hard work

---
+++ oop.c
@@ -0,0 +1,30 @@
+include <iostream>
+
+using std::string;
+
+class Employee {
+public:
+    std::string Name;
+    string Company ;
+    int edad ;
+    void Introduceyourself(){
+        std::cout<< "Name " << Name << std::end1 ;
+    }
+
+};
+
+int main(){
+    int diegos; 
+    Employee employee1;
+    employee1.Name = "diegos ";
+    employee1.Company = " un nuevo etico"
+    employee1.edad = 18;
+
+
+    
+
+employee1.introduceyourself()
+employee1.introduceyourself()
+
+
+}
